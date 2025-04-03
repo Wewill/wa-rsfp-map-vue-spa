1fd2a5c8456a00e4d62932df3fd8ffc143350e30
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-04-03 16:47:22 +0200

    feat: add new SVG icons and update HTML language to French

---
+++ spa3/src/icons/Chevron.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+		<path
+			d="M12.03,17.14c-.27,0-.52-.11-.71-.29L3.04,8.57c-.39-.39-.39-1.02,0-1.41s1.02-.39,1.41,0l7.57,7.57,7.52-7.52c.39-.39,1.02-.39,1.41,0s.39,1.02,0,1.41l-8.22,8.22c-.19.19-.44.29-.71.29Z" />
+	</svg>
+</template>
